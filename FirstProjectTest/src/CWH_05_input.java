c0b9355ac7cdf33f622a5aa9b60887a74e04100c
dsharma2020 <dsmf2018@gmail.com>
2022-10-31 23:46:28 +0530

    add operator files...

--- FirstProjectTest/src/CWH_05_input.java
+++ FirstProjectTest/src/CWH_05_input.java
@@ -4,12 +4,20 @@ public class CWH_05_input {
     public static void main(String[] args) {
         System.out.println("Taking input form the user");
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter number 1");
-        int a = sc.nextInt();
-        System.out.println("Enter number 2");
-        int b = sc.nextInt();
-        int sum = a + b;
-        System.out.println("The sum of the numbers are: ");
-        System.out.println(sum);
+//        System.out.println("Enter number 1");
+//        int a = sc.nextInt();
+//        System.out.println("Enter number 2");
+//        int b = sc.nextInt();
+//        int sum = a + b;
+//        System.out.println("The sum of the numbers are: ");
+//        System.out.println(sum);
+
+//        boolean b1 = sc.hasNextBoolean();
+//        System.out.println(b1);
+//        String str = sc.next();
+//        System.out.println(str);
+          String str1 = sc.nextLine();
+          System.out.println(str1);
+
     }
 }
---
+++ FirstProjectTest/src/CWH_06_operators.java
@@ -0,0 +1,12 @@
+public class CWH_06_operators {
+    public static void main(String[] args) {
+        int a = 4;
+//        int b = 6 + a;
+//        int b = 6 * a;
+//        int b = 6 % a;
+        int b = 9;
+        b += 3;
+        System.out.println(b);
+        System.out.println(6==8);
+    }
+}
