215b6e76942bb296d4871a9d46c7417df514497d
dsharma2020 <dsmf2018@gmail.com>
2022-11-01 22:39:51 +0530

    added string operation file...

---
+++ FirstProjectTest/src/CWH_07_strings.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class CWH_07_strings {
+    public static void main(String[] args) {
+//        String name = new String("test");
+//        String name1 = new String("new test of string...");
+//        System.out.println(name1);
+//        int a = 6;
+//        float b = 5.41255f;
+//        System.out.printf("The value of a is %d and value of b is %f ", a, b);
+//        Scanner sc = new Scanner(System.in);
+//        String st = sc.next();
+//        String st = sc.nextLine();
+//        System.out.println(st);
+
+//        int value = st.length();
+//        System.out.println(value);
+//
+//        String lower_string = st.toLowerCase();
+//        System.out.println(lower_string);
+
+//        String upper_string = st.toUpperCase();
+//        System.out.println(upper_string);
+
+//        String trim_string = st.trim();
+//        System.out.println(trim_string);
+
+//        String sub_string = st.substring(2, (st.length()-2));
+//        System.out.println(sub_string);
+
+//        System.out.println("Length of string is: " + st.length());
+
+//        System.out.println(st.startsWith("haha"));
+//        System.out.println(st.endsWith("wah"));
+
+//        System.out.println(st.charAt(2));
+
+//        System.out.println(st.indexOf("h"));
+
+//        System.out.println(st.equals("test"));
+
+//        System.out.println(st.equalsIgnoreCase("test"));
+
+//        System.out.println("How to use double quotes between string \" use backslash for this");
+
+        System.out.println("How to print backslash \\ use two backslash for one no. printing...");
+
+
+
+    }
+}
